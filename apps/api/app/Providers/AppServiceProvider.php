06fc871253176eeb5d3a36df4a5fde3a422e9070
Arsath Malik <arsathmalik2003@gmail.com>
2026-08-19 06:48:15 +0530

    fix: increase api rate limit to prevent 429s on dashboard

--- apps/api/app/Providers/AppServiceProvider.php
+++ apps/api/app/Providers/AppServiceProvider.php
@@ -67,7 +67,7 @@ class AppServiceProvider extends ServiceProvider
         }
 
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(120)->by($request->user()?->id ?: $request->ip());
+            return Limit::perMinute(1000)->by($request->user()?->id ?: $request->ip());
         });
     }
 }
